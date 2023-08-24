92cabab516e174a26984b6ff6e04156e30f9d5db
Nicky Wang <shuowang823@gmail.com>
2023-08-24 09:35:15 +0800

    feat: add global types for convenience and turn off prop-types lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import { TitleFlex } from '@/components/TitleFlex.tsx';
 import {
   VITE_API_TOKEN,
   VITE_APP_TITLE,
   VITE_APP_VERSION,
 } from '@/utils/const/env.ts';
 
-export const App = () => {
+export const App: FC = () => {
   return (
     <>
-      <div style={{ display: 'flex', alignItems: 'baseline', gap: '8px' }}>
+      <TitleFlex>
         <h2>{VITE_APP_TITLE}</h2>
         <span>{VITE_APP_VERSION}</span>
-      </div>
+      </TitleFlex>
       <p>API TOKEN: {VITE_API_TOKEN}</p>
     </>
   );
