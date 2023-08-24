960f80c9f5c2fc716e1ade8417c3b47ee5dbae63
Nicky Wang <shuowang823@gmail.com>
2023-08-24 09:01:19 +0800

    feat: env setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import {
+  VITE_API_TOKEN,
+  VITE_APP_TITLE,
+  VITE_APP_VERSION,
+} from '@/utils/const/env.ts';
+
 export const App = () => {
-  return <div>Hi, there.</div>;
+  return (
+    <>
+      <div style={{ display: 'flex', alignItems: 'baseline', gap: '8px' }}>
+        <h2>{VITE_APP_TITLE}</h2>
+        <span>{VITE_APP_VERSION}</span>
+      </div>
+      <p>API TOKEN: {VITE_API_TOKEN}</p>
+    </>
+  );
 };
