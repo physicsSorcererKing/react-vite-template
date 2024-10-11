0224d334859a24bad4da71d27052f05ee2d25c21
physicsSorcererKing <psk@psk.rocks>
2024-10-11 08:57:57 +0800

    chore: add dateString to demonstrate eslint react hooks dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useMemo, useState } from 'react';
+
 import { TitleFlex } from '@/components/TitleFlex.tsx';
 import {
   VITE_API_TOKEN,
@@ -6,13 +8,33 @@ import {
 } from '@/utils/const/env.ts';
 
 export const App: FC = () => {
+  const [timestamp, setTimestamp] = useState<number>(Date.now());
+
+  const dateString = useMemo<string>(
+    () =>
+      new Date(timestamp).toLocaleDateString() +
+      ' ' +
+      new Date(timestamp).toTimeString(),
+    [timestamp],
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimestamp(Date.now());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <TitleFlex>
         <h2>{VITE_APP_TITLE}</h2>
         <span>{VITE_APP_VERSION}</span>
       </TitleFlex>
+
       <p>API TOKEN: {VITE_API_TOKEN}</p>
+      <p>{dateString}</p>
     </>
   );
 };
